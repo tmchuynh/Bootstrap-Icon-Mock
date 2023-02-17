4ebc0922287b3400a704876e5faa7177a5e6aecf
Tina Huynh <tinamchuynh@gmail.com>
2023-02-17 15:07:50 -0800

    update

---
+++ public/js/icons.js
@@ -0,0 +1,17 @@
+import * as fs from 'fs';
+ 
+ 
+if (process.argv.length <= 2) {
+    console.log("Usage: " + __filename + " path/to/directory");
+    process.exit(-1);
+}
+ 
+var path = process.argv[2];
+ 
+readdir(path, function(err, items) {
+    console.log(items);
+ 
+    for (var i=0; i<items.length; i++) {
+        console.log(items[i]);
+    }
+});
